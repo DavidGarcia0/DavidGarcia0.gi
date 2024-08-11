58b22ae5a2e1404905d558150fac11f1b40d3ae6
DavidGarcia0 <davidgarciaf0404@gmail.com>
2024-08-11 12:57:48 -0300

    v0.2.0
    JS de la segunda pre-entrega

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 alert("Abra la consola y presione 'Aceptar' para poner en marcha el algoritmo.")
-
 console.log("Buenos Días. Bienvenido a la calculadora de préstamos oficial del Banco Danés.")
 let prestamoPregunta
 let tienePrestamo
@@ -12,53 +11,49 @@ while (prestamoPregunta != "Si" && prestamoPregunta != "No"){
         case "Si":
             tienePrestamo = true
             break
-
         case "No":
             tienePrestamo = false
             break
-
         default:
             console.warn("Responda Si/No")
     }
 } 
-
+class prestamosClase{
+    constructor(prestamoInicial, cuotasPagas, nombre) {
+        this.prestamoInicial = prestamoInicial
+        this.cuotasPagas = cuotasPagas
+        this.nombre = nombre
+    }
+}
+const prestamosArray = []
 function calcularPrestamo(prestamoInicial, cuotasPagas){
-
     let cantidadCuotas
     let cuota
     let seguirCalculando = true
-
+    let nombre = prompt("Cuál es su nombre?")
     while(seguirCalculando == true){
         prestamoInicial = parseInt(prompt("A cuánto equivalía su préstamo Inicial? (en dólares)"))
-
         if (prestamoInicial > 0 && prestamoInicial < 100){
             console.log("Préstamos menores a 100$ son pagados en una sola cuota, de " + prestamoInicial * 1.055 + "$ en su caso.")
             console.log("Tenga un buen día")
             break
-
         } else if (prestamoInicial >= 100){
             for(cantidadCuotas = 1; cantidadCuotas * 100 < prestamoInicial; cantidadCuotas++){
             }
-
             cuota = (prestamoInicial / cantidadCuotas) * 1.055
-
             console.log("Sería un total de  " + cantidadCuotas + " cuotas de " + cuota + "$ cada una. Siendo el monto total, que habrá pagado al cancelar el préstamo, de " + cantidadCuotas * cuota + "$.")
-
                 while(true){
                     cuotasPagas = parseInt(prompt("Cuantas cuotas ha pagado?"))
                     if (cuotasPagas == 0){
-
                         console.log("Le falta por pagar la totalidad del préstamo.")
                         console.log("Tenga un buen día")
                         seguirCalculando = false
                         break
-
                     } else if (cuotasPagas > 0 && cuotasPagas < cantidadCuotas){
                         console.log("Le faltan por pagar " + (cantidadCuotas - cuotasPagas) + " de " + cuota + "$ cada una. La totalidad de lo que debe equivale a " + (cantidadCuotas - cuotasPagas) * cuota + "$.")
                         console.log("Tenga un buen día")
                         seguirCalculando = false
                         break
-
                     } else if (cuotasPagas == cantidadCuotas){
                         console.log("Usted ya ha pagado la totalidad del préstamo, por lo tanto no estaba pendiente.")
                         console.log("Tenga un buen día")
@@ -73,8 +68,17 @@ function calcularPrestamo(prestamoInicial, cuotasPagas){
             console.log("Ingrese una cantidad válida")
         }
     }
+    const prestamo = new prestamosClase (prestamoInicial, cuotasPagas, nombre)
+    prestamosArray.push(prestamo)
+    let nombreDesarrollador = "David"
+    function desarrollador (nombreDesarrollador){
+        let eventoCoincidencia = prestamosArray.find(prestamo => prestamo.nombre === nombreDesarrollador)
+        if (eventoCoincidencia){
+            console.log("Usted es el desarrollador!!")
+        }
+    }
+    desarrollador(nombreDesarrollador)
 }
-
 if (tienePrestamo){
     console.log("Nuestros préstamos se deben de pagar con un 5.5% de interés con respecto al préstamo inicial.")
     calcularPrestamo()
